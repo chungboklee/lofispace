3578880cb71b56ef0327c317d80a78220444b914
sitecollection <lee@collector.org>
2022-04-22 01:19:18 +0900

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', '${{secrets.DB_PASSWORD}}');
+define('DB_NAME', 'account');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
