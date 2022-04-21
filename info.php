b7dcf896d244d63418322415dcfb1529249385ce
sitecollection <lee@collector.org>
2022-04-22 02:14:45 +0900

    Update info.php

--- info.php
+++ info.php
@@ -1,2 +1,15 @@
 <?php
+echo "PHP works!"
+
+//Show all information, defaults to INFO_ALL
 phpinfo();
+
+//gets logo guid
+//php_logo_guid();
+//show version information
+phpversion();
+ 
+ //Show just the module information.
+ //phpinfo(8) yields identical results
+ phpinfo(INFO_MODULES);
+ ?>
